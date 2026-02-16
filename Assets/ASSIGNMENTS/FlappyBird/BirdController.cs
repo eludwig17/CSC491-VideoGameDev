501f90b1d75b923658bca13a0e0e64301596034f
eludwig17 <elijahludwigyt@gmail.com>
2026-02-15 23:26:25 -0800

    script that controls the bird is completed with pressing input of space bar

--- Assets/ASSIGNMENTS/FlappyBird/BirdController.cs
+++ Assets/ASSIGNMENTS/FlappyBird/BirdController.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 
-public class BirdController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class BirdController : MonoBehaviour{
+    [Header("Movement")]
+    [SerializeField] private float flapStrength = 8f;
+    
+    private Rigidbody2D rb;
+    private bool isAlive = true;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (!isAlive) 
+            return;
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Flap();
+        }
+        RotateBird();
+    }
+
+    void Flap(){
+        rb.linearVelocity = Vector2.zero;
+        rb.linearVelocity = new Vector2(0, flapStrength);
+    }
+
+    void RotateBird(){
+        float rotation = Mathf.Lerp(-15, 45, (rb.linearVelocity.y + 10) / 15);
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Pipe") || collision.gameObject.CompareTag("Ground")){
+            Die();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("ScoreZone")){
+            // add game manager add score function here
+        }
+    }
+
+    void Die(){
+        isAlive = false;
+        Debug.Log("You lost Game Over!");
     }
 }
