5e03076c58ea8923be2ec8f82ee18c6d9e74cfb5
eludwig17 <elijahludwigyt@gmail.com>
2026-02-16 14:42:46 -0800

    init of audio script

---
+++ Assets/ASSIGNMENTS/FlappyBird/AudioSource.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AudioSource : MonoBehaviour{
+    private AudioSource audio;
+    
+    // public AudioClip[] clips : 
+    
+    void Start(){
+        
+    }
+    
+    void Update(){
+        
+    }
+}
