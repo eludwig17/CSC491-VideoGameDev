df42735c68c7611236e7c21af065dcb32646b410
eludwig17 <elijahludwigyt@gmail.com>
2026-02-16 01:06:09 -0800

    created spawner script for the pipes

---
+++ Assets/ASSIGNMENTS/FlappyBird/PipeSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour {
+   [Header("Spawn Pipes")]
+   [SerializeField] private GameObject pipePrefab;
+   [SerializeField] private float spawnInterval = 3f;
+   [SerializeField] private float spawnX = 20f;
+   
+   [Header("Randomize Height")]
+   [SerializeField] private float minHeight = -5f;
+   [SerializeField] private float maxHeight = 5f;
+
+   private float timer = 0f;
+   private bool isSpawning = true;
+
+   void Update(){
+      if (!isSpawning)
+         return;
+      timer += Time.deltaTime;
+      if (timer >= spawnInterval){
+         Spawn();
+         timer = 0f;
+      }
+   }
+
+   void Spawn(){
+      float randomY = Random.Range(minHeight, maxHeight);
+      Vector3 spawnPos = new Vector3(spawnX, randomY, 0f);
+      Instantiate(pipePrefab, spawnPos, Quaternion.identity);
+   }
+
+   public void StopSpawn(){
+      isSpawning = false;
+   }
+
+}
