2a1355cede17748f9ba5c0361e4802b734bb38b2
eludwig17 <elijahludwigyt@gmail.com>
2026-02-16 01:05:57 -0800

    updated scene and pipe despawn distance

--- Assets/ASSIGNMENTS/FlappyBird/Pipe.cs
+++ Assets/ASSIGNMENTS/FlappyBird/Pipe.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour{
     [SerializeField] private float movementSpeed = 3f;
-    [SerializeField] private float despawnX = -15f;
+    [SerializeField] private float despawnX = -20f;
     
     void Update(){
         transform.position += Vector3.left * movementSpeed * Time.deltaTime;
