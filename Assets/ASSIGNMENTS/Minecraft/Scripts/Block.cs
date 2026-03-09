50a1451205737d1874daae8cad8248f524a592cc
eludwig17 <elijahludwigyt@gmail.com>
2026-03-08 23:01:04 -0700

    scripts for block prefab

---
+++ Assets/ASSIGNMENTS/Minecraft/Scripts/Block.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Block : MonoBehaviour{
+    public float durabilitySeconds = 1.5f;
+    public ParticleSystem breakingParticlesPrefab;
+    ParticleSystem breakingParticles;
+    float lastBreakProgress;
+    
+    void Update(){
+        if (breakingParticles){
+            if (Time.time > lastBreakProgress + .1f){
+                Destroy(breakingParticles);
+            }
+        }
+    }
+
+    public bool TryBreak(float breakSeconds){
+        lastBreakProgress = Time.time;
+        if (!breakingParticles && breakingParticlesPrefab){
+            breakingParticles = Instantiate(breakingParticlesPrefab);
+            breakingParticles.transform.position = transform.position;
+        }
+
+        if (breakSeconds > durabilitySeconds){
+            Break();
+            return true;
+        }
+        return false;
+    }
+
+    public void Break(){
+        if (breakingParticles){
+            Destroy(breakingParticles);
+        }
+        Destroy(gameObject);
+    }
+}
