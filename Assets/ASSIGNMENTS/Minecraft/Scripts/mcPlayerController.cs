9dee212054b1b7b2e4561e28192e3c4f9df6c7f9
eludwig17 <elijahludwigyt@gmail.com>
2026-03-08 19:53:56 -0700

    finished test scene with player controller

---
+++ Assets/ASSIGNMENTS/Minecraft/Scripts/mcPlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class mcPlayerController : MonoBehaviour {
+    [Header("Player Movement")] 
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 9f;
+    public float jump = 5f;
+    
+    [Header("Mouse Controls")] 
+    public float mouseSense = 5f;
+    public float minPinchClamp = -90f;
+    public float maxPinchClamp = 90f;
+    
+    [Header("Ground Checker")]
+    public float groundCheckDist = 1.1f;
+    public LayerMask groundMask;
+    
+    private Rigidbody rb;
+    private Transform camTransform;
+    private float pitch = 0f;
+    
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        
+        camTransform = GetComponentInChildren<Camera>().transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        MouseMovement();
+        Jump();
+    }
+
+    void FixedUpdate(){
+        Movement();
+    }
+
+    void MouseMovement(){
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense;
+        transform.Rotate(Vector3.up * mouseX);
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minPinchClamp, maxPinchClamp);
+        camTransform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+    }
+
+    void Movement(){
+        float horizonal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical"); 
+        Vector3 moveDir = transform.forward * vertical + transform.right * horizonal;
+        
+        if (moveDir.magnitude > 1f)
+            moveDir.Normalize();
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+        
+        Vector3 targetPos = rb.position + moveDir * speed * Time.fixedDeltaTime;
+        rb.MovePosition(targetPos);
+    }
+
+    void Jump(){
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+    }
+    
+    bool IsGrounded(){
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDist, groundMask);
+    }
+}
