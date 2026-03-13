01964e6f89701f04db9036698eb84d7fb9b04ee4
eludwig17 <elijahludwigyt@gmail.com>
2026-03-13 12:35:51 -0700

    init mesh gen script

---
+++ Assets/ASSIGNMENTS/Minecraft/Scripts/MeshGenerator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour{
+    public Material blankMat;
+    private static Mesh _sharedCubeMesh;
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
