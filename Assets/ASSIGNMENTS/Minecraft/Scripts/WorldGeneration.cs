fab47052ecc4e5760fc8aa67f32ff4d2b4938ae9
eludwig17 <elijahludwigyt@gmail.com>
2026-04-10 01:38:55 -0700

    modified world genn script to use uv and different block types

--- Assets/ASSIGNMENTS/Minecraft/Scripts/WorldGeneration.cs
+++ Assets/ASSIGNMENTS/Minecraft/Scripts/WorldGeneration.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 
-public class WorldGeneration : MonoBehaviour{
-    public GameObject blockPrefab;
-    public int width = 16;
-    public int length = 16;
-    public int height = 3;
+namespace ASSIGNMENTS.Minecraft.Scripts{
+    public class WorldGeneration : MonoBehaviour{
+        public GameObject blockPrefab;
+        public int width = 16;
+        public int length = 16;
+        public int height = 3;
 
-    void Start(){
-    }
+        private CubeSpawner cubeSpawner;
 
-    void GenerateWorld(){
-        for (int x = 0; x < width; x++){
-            for (int z = 0; z < length; z++){
-                for (int y = 0; y < height; y++){
-                    Vector3 position = new Vector3(x, y, z);
-                    Instantiate(blockPrefab, position, Quaternion.identity, transform);
-                }
+        void Awake(){
+            cubeSpawner = GetComponent<CubeSpawner>();
+            if (cubeSpawner == null)
+                Debug.LogError("worldgeneration needs a cubespawner on same gameobject");
+        }
+
+        void Start(){
+            GenerateWorld();
+        }
+
+        void GenerateWorld(){
+            for (int x = 0; x < width; x++)
+            for (int z = 0; z < length; z++)
+            for (int y = 0; y < height; y++){
+                Vector3 pos = new Vector3(x, y, z);
+                CubeType type = GetBlockType(x, y, z);
+                cubeSpawner.SpawnCube(pos, type);
             }
+        }
+
+
+        CubeType GetBlockType(int x, int y, int z){
+            if (y == height - 1) return CubeType.Grass;
+            if (y == height - 2) return CubeType.Dirt;
+            return CubeType.Stone;
         }
     }
 }
